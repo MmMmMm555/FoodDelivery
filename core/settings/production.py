a06f52d9f125f9d2be255f087832ab5fcb22804b
MmMmMm555 <qertygfdsa5@gmail.com>
2023-12-25 11:20:36 +0500

    modified:   core/settings/production.py

--- core/settings/production.py
+++ core/settings/production.py
@@ -21,8 +21,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 CSRF_COOKIE_SECURE = True
 CSRF_TRUSTED_ORIGINS = [
-    "https://chess.uicgroup.tech",
-    "https://chess-dev.uicgroup.tech",
+    "https://example.com",
 ]
 
 ###################################################################
